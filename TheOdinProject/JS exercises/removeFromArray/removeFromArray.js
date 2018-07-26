99d564787a11f39306c63a57113e377e23144e65
Mandy Trinh <>
2018-07-25 20:34:21 -0700

    added non destructive way to do removeFromArray via filter and includes method

--- TheOdinProject/JS exercises/removeFromArray/removeFromArray.js
+++ TheOdinProject/JS exercises/removeFromArray/removeFromArray.js
@@ -1,14 +1,16 @@
-/* 
+
 Implement a function that takes an array and some other arguments then removes the other arguments from that array:
 
 ```javascript
 remove([1,2,3,4], 3) // should remove 3 and return [1,2,4]
 ```
-*/
+
+// DESTRUCTIVE (modifies original array)
 var removeFromArray = function() 
 {
 	// define function with no params
 	// use keyword parameters, which is an array-like object, and its [] index method, to get the Nth parameters
+	// loop through and compare, using splice to remove the elements that are the same
 	let inputArray = arguments[0];
 	let paramLength = arguments.length;
 	let inputArrayLength = inputArray.length;
@@ -28,5 +30,18 @@ var removeFromArray = function()
 	}
 	return inputArray;
 }
+
+// NON-DESTRUCTIVE (returns new array)
+
+/* let removeFromArray = function(...args)
+{
+	// use .filter(), which creates new array w/ elements that pass the test in the callback function
+	// on elements that are NOT in the array
+	// by using the .includes() method which determines whether an array includes a certain element
+	let inputArray = args[0];
+	return resultArray = inputArray.filter(elem => !args.includes(elem));
+	return resultArray;
+} */
+
 
 module.exports = removeFromArray

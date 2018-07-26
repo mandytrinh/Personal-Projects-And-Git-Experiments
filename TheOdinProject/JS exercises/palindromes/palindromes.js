b35e7a528c3deffd7fab912c12e429f6d91bb815
Mandy Trinh <>
2018-07-25 21:47:53 -0700

    did palindrome example

--- TheOdinProject/JS exercises/palindromes/palindromes.js
+++ TheOdinProject/JS exercises/palindromes/palindromes.js
@@ -1,5 +1,14 @@
-var palindromes = function() {
-
+/* Write a function that determines whether or not a given string is a palindrome. */
+var palindromes = function(str) 
+{
+  // make copy of string
+  // split into its characters using [...string] spread syntax 
+  // use the reverse function to reverse the characters
+  // join the characters into a word
+  // compare this version to the inputstring, if equal then return true
+  let compareStrSplit = [...str];
+  let reversedStr = compareStrSplit.reverse().join("");
+  return reversedStr === str;
 }
 
 module.exports = palindromes

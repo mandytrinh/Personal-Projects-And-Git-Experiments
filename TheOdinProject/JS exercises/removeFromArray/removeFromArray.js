e47ed7070e5cf7a8f43d713b1eed32a860c1441b
Mandy Trinh <>
2018-07-25 20:07:06 -0700

    added removeFromArray exercise

--- TheOdinProject/JS exercises/removeFromArray/removeFromArray.js
+++ TheOdinProject/JS exercises/removeFromArray/removeFromArray.js
@@ -1,5 +1,32 @@
-var removeFromArray = function() {
+/* 
+Implement a function that takes an array and some other arguments then removes the other arguments from that array:
 
+```javascript
+remove([1,2,3,4], 3) // should remove 3 and return [1,2,4]
+```
+*/
+var removeFromArray = function() 
+{
+	// define function with no params
+	// use keyword parameters, which is an array-like object, and its [] index method, to get the Nth parameters
+	let inputArray = arguments[0];
+	let paramLength = arguments.length;
+	let inputArrayLength = inputArray.length;
+	
+	for (let i = 0; i < inputArrayLength; i++)
+	{
+		let idx = 1;
+		while (idx < paramLength)
+		{
+			if (inputArray[i] === arguments[idx])
+			{
+				inputArray.splice(i, 1);
+				console.log(inputArray);
+			}
+			idx++;		
+		}
+	}
+	return inputArray;
 }
 
 module.exports = removeFromArray
